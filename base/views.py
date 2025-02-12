2d99be8c1ff25bfb61515c96ced1ad5fb3e695dc
twpowel <tpowel41@charlotte.edu>
2025-02-11 19:20:54 -0500

    user profiles

--- base/views.py
+++ base/views.py
@@ -92,6 +92,14 @@ def room(request, pk):
     context = {'room': room, 'room_messages':room_messages, 'participants':participants}
     return render(request, 'base/room.html', context)
 
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context={'user':user, 'rooms':rooms, 'topics': topics, 'room_messages':room_messages}
+    return render(request, 'base/profile.html', context)
+
 @login_required(login_url="/login")
 def createRoom(request):
     form = RoomForm()
