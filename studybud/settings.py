8b806f8240984e757eb3f2d46aff2f2f70d08555
twpowel <tpowel41@charlotte.edu>
2025-02-18 21:32:13 -0500

    Django REST Framework

--- studybud/settings.py
+++ studybud/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base.apps.BaseConfig'
+    'base.apps.BaseConfig',
+
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
